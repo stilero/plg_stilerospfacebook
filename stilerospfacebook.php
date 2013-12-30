24f93ec5668d8c929db60841841011f66fc56eda
Daniel Eliasson <daniel@stilero.com>
2013-12-30 10:00:19 +0100

    Working fine

--- stilerospfacebook.php
+++ stilerospfacebook.php
@@ -14,38 +14,119 @@ defined('_JEXEC') or die ('Restricted access');
 if(!defined('DS')){
     define('DS', DIRECTORY_SEPARATOR);
 }
+
 define('PATH_LIBRARY', dirname(__FILE__).DS.'library'.DS);
-JLoader::discover('StileroSPFB', PATH_LIBRARY, true, true);
-JLoader::discover('StileroSPFBOauth', PATH_LIBRARY.'oauth', true, true);
-JLoader::discover('StileroSPFBEndpoint', PATH_LIBRARY.'endpoints', true, true);
+JLoader::discover('StileroSPFB', PATH_LIBRARY, false, true);
+JLoader::discover('StileroSPFB', PATH_LIBRARY, false, true);
+JLoader::discover('StileroSPFBOauth', PATH_LIBRARY.'fblibrary'.DS.'fboauth'.DS);
+JLoader::discover('StileroSPFBOauth', PATH_LIBRARY.'fblibrary'.DS.'oauth'.DS);
+JLoader::discover('StileroSPFBEndpoint', PATH_LIBRARY, false, true);
+//JLoader::discover('StileroSPFBEndpoint', PATH_LIBRARY.'fblibrary'.DS.'endpoint'.DS, true);
 //JLoader::discover('StileroSPFBOauth', PATH_LIBRARY.'fboauth', true, true);
 JLoader::register('SocialpromoterImporter', JPATH_ADMINISTRATOR.DS.'components'.DS.'com_socialpromoter'.DS.'helpers'.DS.'importer.php');
 JLoader::register('SocialpromoterPosttype', JPATH_ADMINISTRATOR.DS.'components'.DS.'com_socialpromoter'.DS.'library'.DS.'posttype.php');
 //jimport('joomla.plugin.plugin');
 jimport('joomla.event.plugin');
 
-class plgSystemStilerospfacebook extends JPlugin {
+class plgSocialpromoterStilerospfacebook extends JPlugin {
+    protected $Facebook;
+    protected $AccessToken;
+    protected $Feed;
+    protected $pageId;
+    protected $adminId;
+    protected $_appId;
+    protected $_appSecret;
+    protected $_authToken;
+    protected $_fbpageAuthToken;
     
     const SP_NAME = 'Facebook Plugin';
     const SP_DESCRIPTION = 'Posts links and photos to Facebook';
     const SP_IMAGE = '';
     protected $supportedPosttypes;
-
-    function plgSystemStilerospfacebook ( &$subject, $config ) {
-        parent::__construct( $subject, $config );
+    
+    public function __construct(&$subject, $config = array()) {
+        parent::__construct($subject, $config);
         $language = JFactory::getLanguage();
-        $language->load('plg_system_stilerospfacebook', JPATH_ADMINISTRATOR, 'en-GB', true);
-        $language->load('plg_system_stilerospfacebook', JPATH_ADMINISTRATOR, null, true);
-        SocialpromoterImporter::importLibrary();
-        SocialpromoterImporter::importHelpers();
-        $this->supportedPosttypes = array(
-            SocialpromoterPosttype::LINK, 
-            SocialpromoterPosttype::IMAGE
-        );
+        $language->load('plg_socialpromoter_stilerospfacebook', JPATH_ADMINISTRATOR, 'en-GB', true);
+        $language->load('plg_socialpromoter_stilerospfacebook', JPATH_ADMINISTRATOR, null, true);
+        $this->setParams();
+    }
+        
+    /**
+     * Reads the params and sets them in the class 
+     */
+    protected function setParams(){
+        if(!isset($this->params)){
+            $plg = JPluginHelper::getPlugin('socialpromoter', 'stilerospfacebook');
+            $plg_params = new JRegistry();
+            $plg_params->loadString($plg->params);
+            $this->params = $plg_params;
+        }
+        $this->adminId = $this->params->def('fbadmin_id');
+        $this->pageId = $this->params->def('fb_pages');
+        $this->_appId = $this->params->def('fb_app_id');
+        $this->_appSecret = $this->params->def('fb_app_secret');
+        $this->_authToken = $this->params->def('auth_token');
+    }
+    
+    /**
+     * Checks if the post is to a personal wall or a page. It compares the page id
+     * with the admin id, and if they mat
+     * @return boolean true if personal post
+     */
+    protected function isPersonalPost(){
+            $this->Facebook->User->setUserId('me');
+            $me = $this->Facebook->User->me();
+            $user = StileroSPFBOauthResponse::handle($me);
+            if($user->id == $this->pageId){
+                $this->Facebook->Photos->setUserId('me');
+                $this->params->set('fb_pages', '');
+                StileroSPFBPluginparamshelper::storeParams($this->params, 'autofbook');
+                return true;
+            }
+    }
+    
+    /**
+     * Wraps up after a call. Shows messages and updates tokens
+     * @param string $response JSON response from FB
+     */
+    protected function wrapUp($response){
+        $postResponse = StileroSPFBOauthResponse::handle($response);
+        if(isset($postResponse->id)){
+            return true;
+        }else if($postResponse == null){
+            return false;
+        }else{
+            return false;
+        }
+    }
+    
+    /**
+     * Prepares for a FB Page call
+     */
+    protected function initFBPageCall(){
+        $response = $this->Facebook->User->getTokenForPageWithId($this->pageId);
+        $this->_fbpageAuthToken = StileroSPFBOauthResponse::handle($response);
+        $this->Facebook->Photos->setToken($this->_fbpageAuthToken);
+        $this->Facebook->Photos->setUserId($this->pageId);
     }
     
     public function postImage($url, $title='', $description='', $tags=''){
-        return true;
+        $redirectUri = JURI::root();
+        $this->Facebook = new StileroSPFBFacebook($this->_appId, $this->_appSecret, $redirectUri);
+        $this->Facebook->setAccessTokenFromToken($this->_authToken);
+        $this->Facebook->init();
+        $this->isPersonalPost();
+        if($this->pageId != ''){
+            if(!$this->isPersonalPost()){
+                $this->initFBPageCall();
+            }
+        }else{
+            $this->Facebook->Photos->setUserId('me');
+        }
+        $caption = $description.' '.$tags;
+        $response = $this->Facebook->Photos->publishFromUrl($url, $caption);
+        return $this->wrapUp($response);
     }
     
     /**
